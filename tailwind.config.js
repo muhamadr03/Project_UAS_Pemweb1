74773b8657976118caa7bb9f57f899d55a8d595c
Roisazzshid <isamiq3@gmail.com>
2025-01-23 14:17:08 +0700

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/flyonui/dist/js/accordion.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    require("flyonui"),
+    require("flyonui/plugin")
   ],
 }
